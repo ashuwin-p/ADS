1a708621ecef8147e556c371600c530e5cc551f3
ashuwin-p <ashuwinssn@gmail.com>
2024-06-20 19:51:26 +0530

    Uses OOP

--- EX_10_knap_sack.py
+++ EX_10_knap_sack.py
@@ -11,40 +11,58 @@
 from tabulate import tabulate
 
 
+class Product:
+    def __init__(self, item_no, profit, weight):
+        self.item_no = item_no
+        self.profit = profit
+        self.weight = weight
+        self.ratio = round(profit / weight, 3)
+
+    def __lt__(self, other):
+        return self.ratio < other.ratio
+
+    def __str__(self):
+        return str(self.item_no)
+
+
 class Knap_Sack:
     def __init__(self, M, N):
         self._max_capacity = M
         self._num_products = N
-        self._data = {}
+        self._data = []
 
-    def add_detail(self, product, profit, weight):
-        self._data[product] = (profit, weight, round((profit / weight), 3))
+    def add_product(self, item_no, profit, weight):
+        product = Product(item_no, profit, weight)
+        self._data.append(product)
 
     def solve_KS(self):
-        self._data = dict(
-            sorted(self._data.items(), key=lambda item: item[1][2], reverse=True)
-        )
+        self._data = sorted(self._data, reverse=True)
 
         total_profit = 0
         remaining = self._max_capacity
         details = []
-        for data in self._data.items():
+        for product in self._data:
             if remaining == 0:
                 break
-            product = data[0]
-            profit, weight, unit_profit = data[1]
 
-            if weight > remaining:
-                taken_unit = remaining / weight
+            if product.weight > remaining:
+                taken_unit = remaining / product.weight
             else:
                 taken_unit = 1
 
-            remaining -= weight * taken_unit
+            remaining -= product.weight * taken_unit
 
-            taken_profit = profit * taken_unit
+            taken_profit = product.profit * taken_unit
 
             details.append(
-                [product, profit, weight, taken_unit, remaining, taken_profit]
+                [
+                    product.item_no,
+                    product.profit,
+                    product.weight,
+                    taken_unit,
+                    remaining,
+                    taken_profit,
+                ]
             )
 
             total_profit += taken_profit
@@ -65,15 +83,16 @@ class Knap_Sack:
         print(tabulate(table, headers=header, tablefmt="grid"))
 
     def __str__(self):
-        return str(self._data)
+        return str([str(product) for product in self._data])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     K = Knap_Sack(15, 7)
-    K.add_detail(1, 10, 2)
-    K.add_detail(2, 5, 3)
-    K.add_detail(3, 15, 5)
-    K.add_detail(4, 7, 7)
-    K.add_detail(5, 6, 1)
-    K.add_detail(6, 18, 4)
-    K.add_detail(7, 3, 1)
+    K.add_product(1, 10, 2)
+    K.add_product(2, 5, 3)
+    K.add_product(3, 15, 5)
+    K.add_product(4, 7, 7)
+    K.add_product(5, 6, 1)
+    K.add_product(6, 18, 4)
+    K.add_product(7, 3, 1)
     K.solve_KS()
